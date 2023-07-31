5459e57ea0fab862e61c2e3de714e3fb9fb650ef
rapidmon <ehhwll@hanmail.net>
2023-07-31 17:43:53 +0900

    inter

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-// const logo_back = document.querySelector(".logo_back");
+document.addEventListener("DOMContentLoaded", function(){
+    const main = document.querySelector(".logo-back");
+    const main_title = document.querySelector(".main-title");
 
-// logo_back.addEventListener("click", function() {
-//     logo_back.style.transform = "scale(0.05)";
-// });
-const main = document.querySelector(".logo-back");
-const main_title = document.querySelector(".main-title");
-
-main_title.addEventListener("click", function(){
-    main.style.transform = "translate(3%, -47.5%)";
-})
+    main_title.addEventListener("click", function(){
+        main.style.transform = "translate(3%, -47.5%)";
+    })
+});
